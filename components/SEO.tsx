3f03ea8b1c6d589f7874c9abc7108e0b88eb6c51
Facundo Lavagnino <flavagnino8@gmail.com>
2022-01-14 23:35:03 -0300

    fix styled-comp ssr

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -5,16 +5,6 @@ const SEO = () => {
   return (
     <Head>
       <title>OX Soccer - Marketplace</title>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossOrigin="true"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
-        rel="stylesheet"
-      />
     </Head>
   );
 };
