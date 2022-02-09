be9042b5057294559b2f90335ffb25b858ddd9ac
Facundo Lavagnino <flavagnino8@gmail.com>
2022-02-09 16:56:59 -0300

    fix error handler

--- utils/handleRpcErrors.tsx
+++ utils/handleRpcErrors.tsx
@@ -1,14 +1,14 @@
 import toast from "react-hot-toast";
 
 const handleRpcErrors = (error) => {
-  if (error.error && error.error.code && error.error.code === -32603) {
-    error.error.data &&
-      toast.error(
-        error.error.data.originalError.message.replace(
-          "execution reverted: ",
-          ""
+  if (error.error && error.error.code === -32603) {
+    const data = error.error.data;
+
+    data && data.originalError
+      ? toast.error(
+          data.originalError.message.replace("execution reverted: ", "")
         )
-      );
+      : toast.error(data.message.replace("execution reverted: ", ""));
   } else {
     return;
   }
