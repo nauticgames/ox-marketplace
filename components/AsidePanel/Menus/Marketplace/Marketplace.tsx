33033d46351b7e5a624e126e535da5209b48116f
Facundo Lavagnino <flavagnino8@gmail.com>
2022-02-10 05:27:46 -0300

    styles change

--- components/AsidePanel/Menus/Marketplace/Marketplace.tsx
+++ components/AsidePanel/Menus/Marketplace/Marketplace.tsx
@@ -3,16 +3,13 @@ import React from "react";
 import PanelButton from "../../PanelButton";
 import { MarketplaceItems } from "../../Items";
 import { Grid } from "semantic-ui-react";
-import useWindowSize from "../../../../hooks/useWindowsSize";
 
 const Marketplace = () => {
   const { pathname } = useRouter();
-  const { width } = useWindowSize();
-  const isMobile = width < 768;
 
   return (
     <Grid centered>
-      <Grid.Column width={isMobile ? 16 : 14}>
+      <Grid.Column computer={14} mobile={16} tablet={14}>
         {MarketplaceItems.map(({ label, src, name, path }) => (
           <PanelButton
             path={path}
