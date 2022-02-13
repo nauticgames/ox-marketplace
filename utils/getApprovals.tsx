5df8600dd9df5fda001911243f3b1a33064c6cc2
Facundo Lavagnino <flavagnino8@gmail.com>
2022-02-13 06:19:19 -0300

    changes in getApprovals func

---
+++ utils/getApprovals.tsx
@@ -0,0 +1,30 @@
+import { Moralis } from "moralis";
+
+const OWNER_FILTER = process.env.NODE_ENV === "production" ? "src" : "owner";
+const ATTRIBUTES_VALUE =
+  process.env.NODE_ENV === "production" ? "wad" : "value";
+
+const getApprovals = async (setError, setLastApproval, account) => {
+  try {
+    const query = new Moralis.Query("TokenApprovals");
+    query.equalTo(OWNER_FILTER, account);
+
+    const approvals = await query.find();
+
+    if (!approvals.length) {
+      setLastApproval(0);
+    } else {
+      setLastApproval(
+        process.env.NODE_ENV === "production"
+          ? approvals[approvals.length - 1].attributes.wad
+          : approvals[approvals.length - 1].attributes.value
+      );
+    }
+
+    return;
+  } catch (error) {
+    return setError(true);
+  }
+};
+
+export default getApprovals;
