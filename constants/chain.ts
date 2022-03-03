7c19bdecfe98c492bb321e84d757f19020126b1f
Facundo Lavagnino <flavagnino8@gmail.com>
2022-03-02 22:30:42 -0300

    fix typescript errors

--- constants/chain.ts
+++ constants/chain.ts
@@ -1,5 +1,4 @@
-export const CorrectHexChain: string | "0x38" | "0x61" =
-  process.env.NEXT_PUBLIC_CHAIN_HEX;
+export const CorrectHexChain = process.env.NEXT_PUBLIC_CHAIN_HEX;
 export const CorrectChainId: number = Number(process.env.CHAIN_ID);
 
 //0x38-56	Binance Smart Chain Mainnet
