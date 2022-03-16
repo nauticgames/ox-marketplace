1efb1d55117c5f4e8a5e990570ce4515283da0c7
Facundo Lavagnino <flavagnino8@gmail.com>
2022-03-16 01:24:08 -0300

    fix auth method

--- utils/handleCheckWeb3Installed.ts
+++
@@ -1,8 +0,0 @@
-const handleCheckWeb3Installed = () => {
-  const installed =
-    typeof window !== "undefined" && typeof window.ethereum !== "undefined";
-
-  return installed;
-};
-
-export default handleCheckWeb3Installed;
